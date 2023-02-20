ec83ec93bbf3f7aa974c810e17d8fea9035a296e
damian gwozdz <gwozdziu1017@o2.pl>
2023-02-20 23:02:14 +0100

    Adding implementaion in HumanPlayer.hpp

--- include/HumanPlayer.hpp
+++ include/HumanPlayer.hpp
@@ -9,9 +9,9 @@ class HumanPlayer : public IPlayer
 public:
     HumanPlayer(std::string _name, std::string _sign) : name(_name), sign(_sign), winner(false) {}
     ~HumanPlayer() {}
-    std::string getName() override;
-    std::string getSign() override;
-    bool isWinner() override;
+    std::string getName() override { return name; };
+    std::string getSign() override { return sign; };
+    bool isWinner() override { return winner; };
 private:
     std::string name;
     std::string sign;
